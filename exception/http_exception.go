f970505a58ed79f705ba86412d6a6ed8a7a55fcd
tfkhdyt <tfkhdyt@proton.me>
2024-03-13 11:10:10 +0700

    fix(exception): add error logging to http error functions
    This commit adds error logging to the http error functions. This will help identify the root cause of errors and improve the debugging process.

--- exception/http_exception.go
+++ exception/http_exception.go
@@ -6,26 +6,36 @@ import (
 )
 
 func NewInternalServerError(message string, errs ...error) error {
-	log.Errorw(message, "err", errs[0])
+	if len(errs) > 0 {
+		log.Errorw(message, "err", errs[0])
+	}
 	return fiber.NewError(fiber.StatusInternalServerError, message)
 }
 
 func NewNotFoundError(message string, errs ...error) error {
-	log.Warnw(message, "warn", errs[0])
+	if len(errs) > 0 {
+		log.Warnw(message, "err", errs[0])
+	}
 	return fiber.NewError(fiber.StatusNotFound, message)
 }
 
 func NewBadRequestError(message string, errs ...error) error {
-	log.Warnw(message, "warn", errs[0])
+	if len(errs) > 0 {
+		log.Warnw(message, "err", errs[0])
+	}
 	return fiber.NewError(fiber.StatusBadRequest, message)
 }
 
 func NewUnauthenticatedError(message string, errs ...error) error {
-	log.Warnw(message, "warn", errs[0])
+	if len(errs) > 0 {
+		log.Warnw(message, "err", errs[0])
+	}
 	return fiber.NewError(fiber.StatusUnauthorized, message)
 }
 
 func NewUnauthorizedError(message string, errs ...error) error {
-	log.Warnw(message, "warn", errs[0])
+	if len(errs) > 0 {
+		log.Warnw(message, "err", errs[0])
+	}
 	return fiber.NewError(fiber.StatusForbidden, message)
 }
