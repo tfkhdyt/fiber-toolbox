2f45ee51f434b3449ff796e952cd6d6eb896b40b
tfkhdyt <tfkhdyt@proton.me>
2024-03-13 10:34:11 +0700

    fix(jwt): env key use for jwt generation
    This commit fixes the issue where JWT was being generated using a hard-coded key.
    
    The change introduces the use of `os.Getenv()`, which allows for the JWT key to be set as an environment variable, increasing the security and flexibility of the JWT generation process.

--- jwt/jwt_middleware.go
+++ jwt/jwt_middleware.go
@@ -1,6 +1,8 @@
 package jwt
 
 import (
+	"os"
+
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
@@ -8,11 +10,11 @@ import (
 	"github.com/tfkhdyt/fiber-toolbox/exception"
 )
 
-func JwtMiddleware[T jwt.Claims](jwtKey string) func(c *fiber.Ctx) error {
+func JwtMiddleware[T jwt.Claims]() func(c *fiber.Ctx) error {
 	clm := new(T)
 
 	return jwtware.New(jwtware.Config{
-		SigningKey: jwtware.SigningKey{Key: []byte(jwtKey)},
+		SigningKey: jwtware.SigningKey{Key: []byte(os.Getenv("JWT_ACCESS_KEY"))},
 		Claims:     *clm,
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
 			return exception.NewUnauthenticatedError(err.Error())
