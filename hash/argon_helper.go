a7bc73761997de8bdf06aab92c27804a135dc214
tfkhdyt <tfkhdyt@proton.me>
2024-05-20 07:39:17 +0700

    docs: write comments for hash package

--- hash/argon_helper.go
+++ hash/argon_helper.go
@@ -6,12 +6,16 @@ import (
 	"github.com/tfkhdyt/fiber-toolbox/exception"
 )
 
+// Global variable for Argon2 configuration
 var argon argon2.Config
 
+// init function initializes the Argon2 configuration with default settings
 func init() {
 	argon = argon2.DefaultConfig()
 }
 
+// HashPassword hashes a given password using Argon2 and returns the hashed password
+// If hashing fails, it returns an internal server error
 func HashPassword(password string) (string, error) {
 	hashedPassword, err := argon.HashEncoded([]byte(password))
 	if err != nil {
@@ -21,6 +25,8 @@ func HashPassword(password string) (string, error) {
 	return string(hashedPassword), nil
 }
 
+// VerifyPassword verifies a given password against a hashed password using Argon2
+// If verification fails, it returns an appropriate error (internal server error or bad request error)
 func VerifyPassword(password string, hashedPwd string) error {
 	ok, err := argon2.VerifyEncoded([]byte(password), []byte(hashedPwd))
 	if err != nil {
